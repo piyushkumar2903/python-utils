f74ac36fd9eab86d955d9168ba128686dba1355d
piyushkumar2903 <57697302+piyushkumar2903@users.noreply.github.com>
2021-05-14 16:45:20 +0530

    Create extract_element_from_xml.py

---
+++ extract_element_from_xml.py
@@ -0,0 +1,11 @@
+from xml.etree.ElementTree import ElementTree
+tree = ElementTree()
+root = tree.parse("test.xml")
+for node in root.findall('./Data'):
+  # print(node)
+  for type in list(node):
+    if type.find('ProductNo').text == "12345678":
+      print(type.find('MD5').text)
+      print('found')
+    else:
+      print("not found")
