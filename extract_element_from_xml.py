3200abfcbe767a70642cbc7cfd6def1e1a108cb6
piyushkumar2903 <57697302+piyushkumar2903@users.noreply.github.com>
2021-05-21 22:35:57 +0530

    Update extract_element_from_xml.py

--- extract_element_from_xml.py
+++ extract_element_from_xml.py
@@ -1,8 +1,9 @@
 from xml.etree.ElementTree import ElementTree
 tree = ElementTree()
+#parse the XML file - test.xml
 root = tree.parse("test.xml")
+#iterate over items to find specific product number - 12345678
 for node in root.findall('./Items'):
-  # print(node)
   for type in list(node):
     if type.find('ProductNo').text == "12345678":
       print(type.find('MD5').text)
